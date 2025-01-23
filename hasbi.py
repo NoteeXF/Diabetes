273a1011702c6dcff1a34afa1d5a3fb6d0360f28
Edi Priatama <133779877+NoteeXF@users.noreply.github.com>
2025-01-23 19:13:27 +0700

    Add files via upload

--- hasbi.py
+++ hasbi.py
@@ -33,14 +33,6 @@ def diabetes_prediction(input_data):
     # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
 
-# Creating a function for prediction
-def diabetes_prediction(input_data):
-    # changing the input_data to numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
-
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
