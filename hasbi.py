e84bd8cfe7c5e6e403168bfbe85de8b2b8848d20
Edi Priatama <133779877+NoteeXF@users.noreply.github.com>
2025-01-25 11:35:35 +0700

    Update hasbi.py

--- hasbi.py
+++ hasbi.py
@@ -66,7 +66,7 @@ def main():
 
     # Creating a button for Prediction
     if st.button('Diabetes Test Result'):
-       diagnosis = diabetes_prediction([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
+       diagnosis = diabetes_prediction([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
     st.success(diagnosis)
 
 
